9f8bd12f4882684a940cbd59d3b519435e641414
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2018-08-05 12:51:02 +0300

    Add fonts and styles from admin-dash template

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './index.css';
+import './style.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
