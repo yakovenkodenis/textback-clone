b338d5a2060f49adaa0729e66634f8f70035c00f
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2018-08-05 11:39:30 +0300

    Add needed npm packages and babel plugins

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -144,7 +144,11 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              plugins: ['transform-decorators-legacy'],
+              plugins: [
+                'transform-decorators-legacy',
+                'transform-class-properties',
+                'transform-function-bind'
+              ],
 
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
